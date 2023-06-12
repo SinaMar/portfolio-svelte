d5f35884da66fd644fed8ef0bbb67f0105788d23
Sina <sinamardev@gmail.com>
2023-06-12 11:01:57 +0200

    added SectionHeader

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,24 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import FontTest from './lib/FontTest.svelte'
+  import Font2 from "./lib/Font2.svelte";
+  import SectionHeader from "./lib/SectionHeader.svelte";
+  import NavBar from "./lib/NavBar.svelte";
+  import CssTest from "./lib/CssTest.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <div id="About"></div>
+  <NavBar/>
+  <Font2/>
+  <SectionHeader name="Skills"/>
+  <SectionHeader name="Projects"/>
+  <SectionHeader name="Interests"/>
+  <SectionHeader name="Contact"/>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+<style lang="scss">
+ main {
+   position: relative;
+ }
 </style>
---
+++ src/lib/SectionHeader.svelte
@@ -0,0 +1,175 @@
+<script lang="ts">
+    import {onMount} from "svelte";
+
+    export let name;
+
+    let sh = '30';
+
+
+    let innerWidth = window.innerWidth;
+
+    onMount(() => {
+        function onResize() {
+            innerWidth = window.innerWidth;
+        }
+
+        window.addEventListener('resize', onResize);
+        return () => window.removeEventListener('resize', onResize);
+    });
+</script>
+
+<section id={name}></section>
+<div class="section-heading">
+    <h1>
+        {name}
+    </h1>
+</div>
+<svg height={sh} width={innerWidth} class="triangle">
+    <polygon fill="#21242C" points="0,0 {innerWidth},{sh} 0,{sh}" class="triangle2"/>
+</svg>
+<div id="section-body">
+    <a>Start</a>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    End
+
+</div>
+
+
+<style>
+
+    section {
+        scroll-margin-top: 4rem;
+    }
+
+    #na {
+        background: #21242C;
+    }
+
+
+    a {
+        font-size: 32px;
+        font-weight: bold;
+        /*padding: 30px 0 8px;*/
+        position: relative;
+        z-index: 1;
+    }
+
+    a:hover::before {
+        width: 100%;
+        /*right: 0;*/
+        content: "";
+        position: absolute;
+        /*bottom: 16px;*/
+        bottom: 0;
+        /*width: 100%;*/
+        height: 8px;
+        transform: skew(0deg) translateY(-20%);
+        background: #2EB775;
+
+        left: 20%;
+    }
+
+    a::before {
+        content: "";
+        position: absolute;
+        /*bottom: 16px;*/
+        bottom: 0;
+        /*width: 100%;*/
+        width: 0;
+        height: 8px;
+        transform: skew(0deg) translateY(-20%);
+        background: #2EB775;
+        z-index: -1;
+        transition: width .5s, right 0.5s;
+        left: 20%;
+    }
+
+
+    #hallo {
+        text-decoration: underline;
+        text-decoration-color: white;
+        text-decoration-thickness: 50%;
+        text-decoration-skip-self: inherit;
+        text-underline-offset: 2px;
+    }
+
+    #h {
+        background: #747bff;
+        width: 20px;
+    }
+
+    .triangle {
+        position: relative;
+        top: 6px;
+    }
+
+    .triangle2 {
+        background: white;
+    }
+
+    .section-heading {
+
+        margin-top: 3rem;
+        height: 30px;
+        transform: rotateZ(2deg);
+        /*margin-bottom: 20px;*/
+        position: relative;
+        /*top: -20px;*/
+
+
+        background: #21242C;
+    }
+
+
+    #section-body {
+        background: var(--bg-dark);
+        color: white;
+
+    }
+
+    /*#section-body:before {*/
+    /*    content:"";*/
+
+    /*    position:absolute;*/
+    /*    background: white;*/
+    /*    bottom:-25px;*/
+    /*    left: 0;*/
+    /*    height: 50px;*/
+    /*    width: 100%;*/
+    /*    transform: skew(0, -2deg);*/
+    /*}*/
+
+    h1 {
+        font-family: var(--font-heading);
+        transform: rotateZ(-2deg);
+
+        position: absolute;
+        /*text-align: center;*/
+        bottom: 20px;
+        /*z-index: 1;*/
+        right: 20%;
+        margin: 0;
+    }
+</style>
