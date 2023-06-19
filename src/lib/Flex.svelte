0f7c2b221d45076bdd5074533ef0b0b000334487
Sina <sinamardev@gmail.com>
2023-06-19 11:27:19 +0200

    added flex comp

---
+++ src/lib/Flex.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import cssVars from 'svelte-css-vars';
+    import type {justifyContent, position} from "../types/css";
+    export let justifyContent: justifyContent = 'center';
+    export let position: position = 'initial';
+    export let style = '';
+
+    let styleVars = {
+        justifyContent,
+        position
+    }
+</script>
+
+
+
+<div use:cssVars={styleVars} style={style} >
+    <div style:position="absolute"></div>
+    <slot/>
+</div>
+
+
+<style lang="scss">
+    div {
+      //background-color: green;
+        display: flex;
+        justify-content: var(--justify-content);
+    }
+</style>
---
+++ src/types/css.ts
@@ -0,0 +1,7 @@
+type global = 'inherit' | 'initial' | 'revert' | 'revert-layer' | 'unset'; 
+
+export type justifyContent = 'center' | 'start' | 'end' |  'flex-start' | 'flex-end' | 'left' | 'right' | 'space-between' |
+    'space-around' | 'space-evenly' | 'stretch';
+
+export type position = global | 'static' | 'relative' | 'fixed' | 'absolute' | 'sticky';
+
