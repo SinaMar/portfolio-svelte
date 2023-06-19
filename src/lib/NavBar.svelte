f792d8c64cfb962147712ccf94be1584d10761cf
Sina <sinamardev@gmail.com>
2023-06-19 11:27:32 +0200

    navbar

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -4,66 +4,130 @@
 
     let navbar: HTMLElement;
     let offset: number;
-    let sticky = false;
-    let current = "";
+    let sticky;
     let sections;
-    let navLi;
+    let y = 0;
 
+    let target;
+    // Create a new Intersection Observer instance
+    // const observer = new IntersectionObserver(entries => {
+    //     entries.forEach(entry => {
+    //         // Check if the target element is intersecting with the viewport
+    //         if (entry.isIntersecting) {
+    //             // Perform actions when the element is in view
+    //             sticky = false;
+    //             console.log('Element is in view');
+    //         } else {
+    //             sticky = true;
+    //
+    //             // Perform actions when the element is not in view
+    //             console.log('Element is out of view');
+    //         }
+    //     });
+    // });
+
+    // Start observing the target element
+
+
+    let sectionMap = {};
 
     onMount(() => {
-        offset = navbar.offsetTop + 8;
+        // observer.observe(target);
+
+        offset = navbar.offsetTop + 20;
         sections = document.querySelectorAll("section");
-        navLi = document.querySelectorAll("nav #nav-content a");
-        console.log(navLi)
+        sections.forEach( x => {
+            //TODO: window.innerHeight dynamic, since it can change
+            sectionMap[x.getAttribute("id")] = x.offsetTop - (window.innerHeight/2);
+        })
+        console.log(sections);
+        // navLi = document.querySelectorAll("nav #nav-content a");
+
+        // const onScroll = () => {
+        //     sticky = window.scrollY >= offset;
+        //     console.log('scrollY',scrollY);
+        //     // console.log('offset', offset);
+        //
+        //     sections.forEach((section) => {
+        //         const sectionTop = section.offsetTop;
+        //         if (pageYOffset >= sectionTop - 200) {
+        //             current = section.getAttribute("id");
+        //             console.log(current)
+        //         }
+        //     });
+        //
+        // };
 
     })
 
-    window.onscroll = () => {
-        sticky = window.pageYOffset >= offset;
 
-        sections.forEach((section) => {
-            const sectionTop = section.offsetTop;
-            if (pageYOffset >= sectionTop - 64) {
-                current = section.getAttribute("id");
-            }
-        });
-
-
-    };
+    $: sticky = y >= offset;
+    // $: about = y >= 100 && y <= 500;
 
 </script>
 
-<div id="nav-container" class:sticky>
+<svelte:window bind:scrollY={y} />
+
+<!--<div bind:this={target} class="target"></div>-->
+<div id="nav-container" class="desktop">
     <img src={Logo} style={  `height: ${sticky ? 30 : 60}px`} class="logo" alt="Logo"/>
-    <nav id="navbar" bind:this={navbar}>
+    <nav id="navbar" bind:this={navbar} class:sticky>
         <div id="nav-content">
-            <a href="#About" class:active={current==="About"}>.About</a>
-            <a href="#Skills" class:active={current==="Skills"}>.Skills</a>
-            <a href="#Projects" class:active={current==="Projects"}>.Projects</a>
-            <a href="#Interests" class:active={current==="Interests"}>.Interests</a>
-            <a href="#Contact" class:active={current==="Contact"}>.Contact</a>
+            <a href="#" class:active={ y < sectionMap['Skills']}>.About</a>
+            <a href="#Skills" class:active={y >= sectionMap['Skills']  && y < sectionMap['Projects']}>.Skills</a>
+            <a href="#Projects" class:active={y >= sectionMap['Projects'] && y < sectionMap['Interests']}>.Projects</a>
+            <a href="#Interests" class:active={y >= sectionMap['Interests'] && y < sectionMap['Contact']}>.Interests</a>
+            <a href="#Contact" class:active={y >= sectionMap['Contact'] }>.Contact</a>
         </div>
 
     </nav>
 </div>
 
 
+<!--<div class="burger mobile">-->
+
+<!--</div>-->
+
 <style>
+
+    .target {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 1px;
+        background: white;
+        width: 100%;
+        z-index: -10;
+    }
+
+    .burger {
+        background: var(--bg);
+        width: 50px;
+        height: 50px;
+        position: fixed;
+        right: 0;
+        top: 0;
+        z-index: 10;
+
+    }
+
+    .sticky {
+        /*background: var(--accent-color)!important;*/
+        border-bottom: 2px solid var(--accent-color);
+        transition: background-color 0.7s;
+    }
+
 
     .logo {
         margin-right: 1rem;
         transition: height 0.3s;
     }
 
-    .sticky {
-        position: fixed;
-        top: 0;
-        z-index: 10;
-        margin-top: 0 !important;
-    }
-
     #nav-container {
-        margin-top: 1rem;
+        position: sticky;
+        z-index: 10;
+        top: 0;
+        /*margin-top: 2rem;*/
         display: flex;
         align-items: center;
         width: 100%;
@@ -83,8 +147,9 @@
         background: var(--bg-dark);
         width: 100%;
         margin-right: 0;
-        transition: width .3s;
+        transition: width .3s, background-color 0.2s;
         height: fit-content;
+
     }
 
     nav::before {
@@ -97,6 +162,7 @@
         position: absolute;
         transform: skew(20deg) translateX(-50%);
         background: var(--bg-dark);
+
     }
 
     a {
@@ -143,5 +209,26 @@
         z-index: -1;
         transition: width .3s, right 0.3s;
         left: 20%;
+    }
+
+    @media screen and (min-width: 613px) {
+        .desktop {
+            display: block;
+        }
+
+        .mobile {
+            display: none;
+        }
+    }
+
+    /* Mobile styles */
+    @media screen and (max-width: 612px) {
+        .desktop {
+            display: none !important;
+        }
+
+        .mobile {
+            display: block;
+        }
     }
 </style>
