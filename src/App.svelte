a328a4804e1151e3180875dbd344648b3259bbcd
Sina <sinamardev@gmail.com>
2023-06-19 11:28:07 +0200

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,76 @@
 <script lang="ts">
-  import SectionHeader from "./lib/SectionHeader.svelte";
-  import NavBar from "./lib/NavBar.svelte";
-  import About from "./sections/About.svelte";
-  import Skills from "./sections/Skills.svelte";
-  import CssTest from "./lib/CssTest.svelte";
+    import SectionHeader from "./lib/SectionHeader.svelte";
+    import NavBar from "./lib/NavBar.svelte";
+    import About from "./sections/About.svelte";
+    import Skills from "./sections/Skills.svelte";
+    import CssTest from "./lib/Carousel.svelte";
+    import Projects from "./components/projects/ProjectsSection.svelte";
+    import Test from "./lib/Carousel.svelte";
+    import Test1 from "./lib/Test1.svelte";
+    import Test2 from "./lib/Test2.svelte";
 </script>
 
 <main>
 
-  <NavBar/>
-  <About/>
-  <SectionHeader name="Skills">
-    <Skills/>
-  </SectionHeader>
+    <NavBar/>
+    <About/>
+    <SectionHeader name="Skills">
+        <Skills/>
+    </SectionHeader>
+    <SectionHeader name="Projects" mirrored>
+        <Projects/>
+    </SectionHeader>
+    <SectionHeader name="Interests">
+        <div>
+            asperiores beatae consequatur dicta distinctio excepturi fugit illum, in iusto nisi possimus praesentium
+            quas saepe sapiente, sit! Aliquid, amet architecto atque deserunt dolor, doloremque enim, error est eveniet
+            hic modi repellat sequi voluptas! Adipisci asperiores corporis dignissimos eius ex explicabo fuga id illo
+            illum ipsa, iure iusto maiores minus, neque nesciunt nulla porro quisquam recusandae rem suscipit unde velit
+            voluptate! Commodi cum eveniet harum in ipsum magni nemo praesentium reprehenderit voluptate voluptatibus?
+            Amet aperiam autem est necessitatibus, odio quaerat repudiandae sunt vitae voluptatem. Accusamus animi autem
+            beatae consectetur, consequuntur cupiditate distinctio dolore dolorum ea earum enim est excepturi expedita
+            explicabo harum illum laborum libero minus mollitia necessitatibus nemo nostrum numquam officia officiis
+            pariatur possimus praesentium quia quidem quod quos sapiente tempora temporibus unde? Aperiam illum placeat
+            quod quos vel. Atque, aut beatae commodi ducimus earum error explicabo ipsa ipsam iusto, maiores modi
+            nesciunt nihil porro quod similique ullam unde. Aperiam consequuntur corporis, dignissimos explicabo fuga
+            ipsam labore placeat quidem quis quod suscipit tenetur voluptate voluptatem! Exercitationem, laboriosam,
+            molestias! A architecto assumenda at beatae blanditiis commodi cupiditate delectus distinctio dolor ducimus
+            ea eaque esse eveniet excepturi expedita harum hic incidunt laboriosam laborum minus modi molestiae
+            molestias natus nisi omnis optio, perferendis porro praesentium quas quibusdam quos repudiandae, sunt
+            temporibus ut velit veritatis vitae. Amet aperiam asperiores aut consequuntur cum debitis eum exercitationem
+            harum, maxime, nulla odio quaerat quam, qui sit veritatis. Est impedit modi necessitatibus neque, omnis
+            perferendis quas saepe sequi. A blanditiis commodi corporis cumque deleniti, doloribus impedit minima,
+            neque, pariatur repellat suscipit vero. Hic incidunt inventore sed tempore. Ad, alias eligendi est facere
+            laborum nam nemo non officia quis quos reiciendis repudiandae saepe sint. A cum debitis, doloribus ex
+            expedita hic id, nam nemo quas quia quo repudiandae vitae voluptate!
+        </div>
+    </SectionHeader>
+    <SectionHeader name="Contact" mirrored>
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto at blanditiis cupiditate
+            deserunt dolore, error exercitationem labore laboriosam nemo nihil quibusdam quo recusandae sunt tenetur
+            ullam unde. Blanditiis id molestiae nesciunt placeat voluptatibus. Accusantium, beatae deserunt, eius ex
+            explicabo fugiat harum id minima odit perferendis perspiciatis porro praesentium rerum sapiente tempora
+            ullam velit? Dicta doloremque esse eveniet fugiat, harum illum, mollitia nihil odio tempora veniam vero,
+            voluptatum! Ab aperiam commodi consectetur delectus dignissimos, ea eligendi eveniet, fuga hic illum labore
+            laboriosam minima minus neque quia quibusdam rem repellat tempore unde, voluptates. Architecto aspernatur
+            dicta excepturi, possimus quas quia repudiandae!
+        </div>
+    </SectionHeader>
 
 
+    <Test1/>
+    <Test2/>
+    
 
 
-<!--  <SectionHeader name="Projects"/>-->
-<!--  <SectionHeader name="Interests"/>-->
-<!--  <SectionHeader name="Contact"/>-->
+    <!--  <SectionHeader name="Projects"/>-->
+    <!--  <SectionHeader name="Interests"/>-->
+    <!--  <SectionHeader name="Contact"/>-->
 </main>
 
 <style lang="scss">
- main {
-   //position: relative;
- }
+  main {
+    //position: relative;
+  }
 </style>
--- src/sections/About.svelte
+++ src/sections/About.svelte
@@ -1,32 +1,35 @@
 <script lang="ts">
     import Portrait from '../assets/Portrait.png'
-    import HandWaveIcon from '../assets/handwave.svg'
+    import HandWaveIcon from '../assets/icons/handwave.svg'
 </script>
 
 
 <section id="About">
-    <div class="about-container">
-        <div class="about">
-            <div>
-                <h2>Hey there,<img src={HandWaveIcon} alt="HandWave Icon"/></h2>
-                <h1>I’m <span>Sina</span></h1>
-                <h1 class="backline">Frontend Developer</h1>
-                <p>I'm a dedicated Frontend Developer crafting outstanding websites. I stay updated with the latest
-                    trends and
-                    blend functionality with aesthetics for seamless user experiences. Collaboration is key to my
-                    process,
-                    bringing diverse perspectives for exceptional digital solutions. Let's build user-friendly websites
-                    together that leave a lasting impression!
-                </p>
-                <div class="buttons">
-                    <button class="primary">Download CV</button>
-                    <button class="secondary">See Project</button>
-                </div>
+</section>
+
+<div class="about-container">
+    <div class="about">
+        <div>
+            <h2>Hey there,<img src={HandWaveIcon} alt="HandWave Icon"/></h2>
+            <h1>I’m <span>Sina</span></h1>
+            <h1 class="backline">Frontend Developer</h1>
+            <p>I'm a dedicated Frontend Developer crafting outstanding websites. I stay updated with the latest
+                trends and
+                blend functionality with aesthetics for seamless user experiences. Collaboration is key to my
+                process,
+                bringing diverse perspectives for exceptional digital solutions. Let's build user-friendly websites
+                together that leave a lasting impression!
+            </p>
+            <div class="buttons">
+                <!--                    <button class="primary">Download CV</button>-->
+                <a class="btn primary">Download CV</a>
+                <a class="btn secondary">See Project</a>
+                <!--                    <button class="secondary">See Project</button>-->
             </div>
         </div>
-        <img src={Portrait} alt="Logo" height="280px"/>
     </div>
-</section>
+    <img src={Portrait} alt="Logo" height="280px"/>
+</div>
 
 
 <style lang="scss">
@@ -34,8 +37,9 @@
   .about-container {
     column-gap: 12.5rem;
     margin: auto;
-    margin-top: 6rem;
-    margin-bottom: 20rem;
+    //margin-top: 6rem;
+    height: calc(100vh - 4rem);
+    //margin-bottom: 20rem;
     display: flex;
     align-items: center;
     text-align: center;
@@ -99,27 +103,157 @@
   }
 
   .buttons {
+    display: flex;
+    gap: 1rem;
     margin-top: 4rem;
     text-align: right;
+    justify-content: right;
+  }
+
+  *,
+  *:before,
+  *:after {
+    transition: 0.3s ease-in-out;
+  }
+
+  *:before,
+  *:after {
+    z-index: -1;
+  }
+
+  .btn {
+    display: block;
+    position: relative;
+    width: fit-content;
+    border-radius: 0.375rem 1.25rem;
+    padding: 0.625rem 2.75rem;
+    font-size: 1rem;
+    cursor: pointer;
+
+    overflow: hidden;
+
+
+    &.primary {
+      color: white;
+      border: 1px solid transparent;
+      background: var(--accent-color);
+    }
+
+    &.secondary {
+      border: 1px solid var(--accent-color);
+      color: var(--accent-color);
+    }
+    &.primary:before {
+      content: '';
+      position: absolute;
+
+      top: 0;
+      right: 0px;
+      bottom: 0;
+      left:  -30%;
+      width: 130%;
+      background: var(--accent-color);
+      //background: yellow;
+      transform: skew(30deg) translateX(0);
+      //transition: transform 0.5s ease-in-out;
+
+    }
+    &.primary:after {
+      content: '';
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      width: 120%;
+      position: absolute;
+      background: var(--bg-dark);
+
+      transform: skew(30deg) translateX(-110%);
+      //transition: transform 0.5s ease-in-out;
+    }
+
+
+
+    &.secondary:after {
+      content: '';
+      top: 0;
+      right: -50px;
+      bottom: 0;
+      left: 0;
+      width: 120%;
+      position: absolute;
+      border-right: 50px solid transparent;
+      //border-bottom: 50px solid var(--accent-color);
+      border-bottom: 50px solid var(--bg-dark);
+      transform: translateX(-100%);
+    }
+
+
+    &:hover {
+
+      &.primary {
+        position: relative;
+        transform: translateY(-10%);
+        //transition: transform 0.5s ease-in-out;
+        background: transparent;
+        border-color: var(--accent-color);
+        //border-color: white;
+        // color: var(--accent-color);
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+      }
+
+      &.secondary {
+        color: white;
+      }
+
+      &:after {
+        transform: skew(30deg) translateX(-10%);
+        //transition: transform 0.5s ease-in-out;
+
+      }
+
+      &:before {
+        transform: skew(30deg) translateX(100%);
+        //transition: transform 0.5s ease-in-out;
+
+
+      }
+    }
+
+
+
+    &:active {
+      //transform: translateY(30%);
+      background-color: black;
+      top: 1px;
+    }
+
   }
 
   button {
-    border: none;
+    //border: none;
     border-radius: 0.375rem 1.25rem;
     padding: 0.625rem 2.75rem;
     font-size: 1rem;
     margin-left: 0.25rem;
     cursor: pointer;
+    border: 1px solid transparent;
 
     &.primary {
       background: linear-gradient(93.18deg, #35A770 0%, #25865e 100%);
+      //background: green;
+
+
     }
 
-    &.primary:hover {
-      background: transparent;
-      border: 1px solid;
-      border-color: var(--accent-color);
-    }
+    //
+    //&.primary:hover {
+    //  background: transparent;
+    //  border: 1px solid;
+    //  //background: linear-gradient(93.18deg, #fff 0%, #25865e 100%);
+    //
+    //  border-color: var(--accent-color);
+    //}
 
     &.secondary {
       border: 1px solid;
